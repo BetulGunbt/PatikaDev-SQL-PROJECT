689b362990d8d9d84e408dd3213c7a8af30c21df
BetulGunbt <betul.gunbati@gmail.com>
2023-05-01 08:50:39 +0300

    dvdrental patika.dev odev
    odevin devamı

--- dvdrental patika.dev odev.sql
+++ dvdrental patika.dev odev.sql
@@ -5,3 +5,10 @@ select*from film where length>60 and length<75;
 select*from film where rental_rate=0.99 and replacement_cost=12.99 or replacement_cost=28.99;
 select first_name, last_name from customer where first_name='Mary';
 select*from film where not (length>50 or rental_rate=2.99 or rental_rate=4.99);
+
+select*from film
+where replacement_cost between 12.99 and 16.99;
+select first_name, last_name from actor
+where first_name in ('Penelope', 'Nick', 'Ed');
+select*from film
+where rental_rate in(0.99, 2.99, 4.99) and replacement_cost in(12.99, 15.99, 28.99);
